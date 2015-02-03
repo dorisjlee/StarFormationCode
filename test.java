c61def9d733c4f777e6990e972242b87ff9b1745
dorislee0309 <dorislee@berkeley.edu>
2015-02-02 21:18:43 -0800

    added notebook

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        float Array1[] = {1,2,3};
+        System.out.println(Array1.length);
+        System.out.println(type(Array1));
+    }
+
+}
