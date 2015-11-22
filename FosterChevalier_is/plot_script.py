d1474fefe34d0bebc2bef8421c6a5c040986b916
Doris Lee <dorislee@berkeley.edu>
2015-11-22 14:07:49 -0800

    density plots

--- FosterChevalier_is/plot_script.py
+++ FosterChevalier_is/plot_script.py
@@ -7,4 +7,4 @@ os.chdir("/project/projectdirs/astro250/doris/ramses3/trunk/ramses/bin/")
 for i in np.arange(1,10):
 #    if i%10==0:
 #        plot_time_slice("density", i)
-    plot_is.plot_time_slice("radial_velocity", i,save=True)
+    plot_is.plot_time_slice("density", i,save=True)
