932f9d5c7592910248cd8da30fa71d7f7d0ba79c
Doris Lee <dorislee@berkeley.edu>
2015-11-22 14:03:52 -0800

    transfering png because ipython notebook doesn't work and yt odenst work

--- FosterChevalier_is/plot_is.py
+++ FosterChevalier_is/plot_is.py
@@ -10,9 +10,11 @@ from IPython.core.pylabtools import figsize, getfigs
 from pylab import *
 from numpy import *
 import yt
+print yt.__version__
+print np.__version__
 yt.funcs.mylog.setLevel(50) #coerce output null
 
-def plot_time_slice(physical_quantity,timestep,text="",title="",zoom_factor=""):
+def plot_time_slice(physical_quantity,timestep,text="",title="",zoom_factor="",save=False):
     ds= yt.load("output_{0}/info_{0}.txt".format(str(timestep).zfill(5)))
     slc = yt.SlicePlot(ds, "z",physical_quantity)
     slc.set_axes_unit('pc')
@@ -27,8 +29,10 @@ def plot_time_slice(physical_quantity,timestep,text="",title="",zoom_factor=""):
     slc.annotate_text((0.05, 0.05),"timestep: {}".format(timestep), coord_system='axis')
     slc.annotate_text((0.05, 0.02),"time: {} Myrs".format(timestep*61793.091/1000000.), coord_system='axis')
     slc.annotate_velocity()
-    slc.annotate_grids()
-    slc.show()
+#    slc.annotate_grids()
+    if (save):
+       name  =str(timestep)#physical_quantity[:3]+str(timestep)
+       slc.save(name)
 
 def density_radial_profile(timestep):
     ds= yt.load("output_{0}/info_{0}.txt".format(str(timestep).zfill(5)))
---
+++ FosterChevalier_is/plot_script.py
@@ -0,0 +1,10 @@
+import plot_is
+import os
+import yt 
+import numpy as np
+yt.funcs.mylog.setLevel(50)
+os.chdir("/project/projectdirs/astro250/doris/ramses3/trunk/ramses/bin/")
+for i in np.arange(1,10):
+#    if i%10==0:
+#        plot_time_slice("density", i)
+    plot_is.plot_time_slice("radial_velocity", i,save=True)
